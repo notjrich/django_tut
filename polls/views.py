408107c0247a34b114a0eb612715a6c536ed7887
notjrich <j@notjrich.com>
2016-01-12 12:41:31 -0500

    Updated polls/views.py to incorporate template. Commented out url(...) calls from polls/views.py. I am not sure this is the correct method.

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
-url(r'(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+#url(r'(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
 #example url /polls/5/vote/
-url(r'(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+#url(r'(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 
 #import HttpResponse
 from django.http import HttpResponse
+from django.template import loader
+
+#import Models
+from .models import Question
 
 # Create your views here.
 
 def index(request):
- return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+            'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You're looking a question %s." % question_id)
